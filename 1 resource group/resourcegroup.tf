962a1e747b6357058e8b8286780ebe57deec8b56
Jen Sheerin <jensheerin@microsoft.com>
2021-11-11 09:44:25 -0500

    add features block

--- 1 resource group/resourcegroup.tf
+++ 1 resource group/resourcegroup.tf
@@ -1,6 +1,10 @@
+provider "azurerm" {
+  features {}
+}
+
 # Create AVD Resource Group
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
 resource "azurerm_resource_group" "rg" {
-name     = avd-rg
-location = eastus
+  name     = "avd-rg"
+  location = "eastus"
 }
