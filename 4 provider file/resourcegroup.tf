77a443e4f13b1ba7d69d221e020e74c2e97bb42a
Jen Sheerin <jensheerin@microsoft.com>
2021-11-12 11:30:09 -0500

    add provider block

--- 4 provider file/resourcegroup.tf
+++ 4 provider file/resourcegroup.tf
@@ -1,5 +1,10 @@
+provider "azurerm" {
+  features {}
+}
+
 # Create AVD Resource Group
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
 resource "azurerm_resource_group" "rg" {
-  name     = var.rgname
-  location = var.location
+name     = var.rgname
+location = var.location
 }
